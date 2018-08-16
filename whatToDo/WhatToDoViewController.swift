f4fe0dfa3b3f03d79644740479173aa4d37d9fc8
Biswajit <thisisbish@gmail.com>
2018-08-16 14:30:52 -0400

    To Do complete with DataSource and Delegate

---
+++ whatToDo/WhatToDoViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  whatToDo
+//
+//  Created by Biswajit Sarker on 8/16/18.
+//  Copyright © 2018 UnHunch. All rights reserved.
+//
+
+import UIKit
+
+class WhatToDoViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    //MARK: - Tableview DataSource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+        
+    }
+    
+    //MARK: - TableView delegate Methods
+    
+    // Get the selected Row
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        // Check if there is any checkmark, if not then add a checkmark when selected
+        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        // Deselect row with animation
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+        
+    }
+
+}
+
