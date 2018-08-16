7f52cf659f64a576547286c3e348e4a1088fc8ee
Biswajit <thisisbish@gmail.com>
2018-08-16 13:10:36 -0400

    Initial Commit

---
+++ whatToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  whatToDo
+//
+//  Created by Biswajit Sarker on 8/16/18.
+//  Copyright © 2018 UnHunch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
